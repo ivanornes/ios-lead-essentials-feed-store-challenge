f4647c89ca77482332672ae333daa84e79809530
Ivan <ivan.ornes@gmail.com>
2021-02-08 21:32:27 +0100

    Created `CouchFeedStore` that implements the `FeedStore` protocol

---
+++ FeedStoreChallenge/CouchFeedStore.swift
@@ -0,0 +1,25 @@
+//
+//  CouchFeedStore.swift
+//  FeedStoreChallenge
+//
+//  Created by Ivan Ornes on 8/2/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+import CouchbaseLiteSwift
+
+public class CouchFeedStore: FeedStore {
+	
+	public init() {
+	}
+	
+	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+	}
+	
+	public func retrieve(completion: @escaping RetrievalCompletion) {
+	}
+	
+	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+	}
+}
